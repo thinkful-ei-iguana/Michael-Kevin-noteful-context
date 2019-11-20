f4502dca2ebb2e3c66b661d390ff50fedc4aeddb
Michael Weedman <michael.weedman@gmail.com>
2019-11-20 11:24:59 -0700

    Added some beginning groundwork for NoteView

--- src/components/NoteView.js
+++ src/components/NoteView.js
@@ -3,28 +3,31 @@ import NotFound from "./NotFound";
 import Note from "./Note";
 import Context from "../Context";
 
-export default function NoteView(props) {
-  if (!props.notes) {
-    return <NotFound />;
-  } else {
-    const folderId = this.props.match.params.folderId;
-    const folderMatch = props.folders.find(folder => folder.id === folderId);
-    return (
-      <div id="wrapper">
-        <section>
-          <button onClick={() => props.history.goBack()}>Back</button>
-          <h4>{folderMatch.name}</h4>
-        </section>
-        <main>
-          <Note
-            name={props.notes.name}
-            key={props.notes.id}
-            modified={props.notes.modified}
-            id={props.notes.id}
-          />
-          <p>{props.notes.content}</p>
-        </main>
-      </div>
-    );
+export default class NoteView extends React.Component {
+  static contextType = Context;
+  render() {
+    if (!props.notes) {
+      return <NotFound />;
+    } else {
+      const folderId = this.props.match.params.folderId;
+      const folderMatch = props.folders.find(folder => folder.id === folderId);
+      return (
+        <div id="wrapper">
+          <section>
+            <button onClick={() => props.history.goBack()}>Back</button>
+            <h4>{folderMatch.name}</h4>
+          </section>
+          <main>
+            <Note
+              name={props.notes.name}
+              key={props.notes.id}
+              modified={props.notes.modified}
+              id={props.notes.id}
+            />
+            <p>{props.notes.content}</p>
+          </main>
+        </div>
+      );
+    }
   }
 }
